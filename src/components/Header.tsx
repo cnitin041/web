98cd5fd1bbf3715135f6aa50b55127bc7d340121
cnitin041 <cnitin041@gmail.com>
2026-07-11 19:41:01 +0530

    Fixed path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
           <div
             className="hero-image"
             style={{
-              backgroundImage: "url('/nitin.jpg')",
+              backgroundImage: "url('/Nitin.jpg')",
               backgroundSize: 'cover',
               backgroundPosition: 'center',
             }}
